0434b000807d2d56e8b34f918f6f3bb9ee108f24
Yif Swery <yiftachswr@gmail.com>
2016-05-29 11:42:45 +1200

    Add placeholders for rending all the rr DNS types

--- src/common/js/script.js
+++ src/common/js/script.js
@@ -64,25 +64,83 @@ KangoAPI.onReady(function() {
 
     function displayRecords(data)
     {
-        log('Rending Records');
+        log('Processing record display');
         $('.loader').hide();
 
         var active = 'active';
-        $.each(data.dns_records, function( index, value ){
-            var htmlUl = '<li role="presentation" class="' + active + '"><a href="#table-results-' + index + '" aria-controls="table-results-' + index + '" role="tab" data-toggle="tab" aria-expanded="true">' + index + '</a></li>';
+        $.each(data.dns_records, function( rrType, records ){
+            var htmlUl = '<li role="presentation" class="' + active + '"><a href="#table-results-' + rrType + '" aria-controls="table-results-' + rrType + '" role="tab" data-toggle="tab" aria-expanded="true">' + rrType + '</a></li>';
             $("#records-tab").append(htmlUl);
 
-            var tabbedContentHtml = '<div role="tabpanel" class="tab-pane ' + active + '" id="table-results-' + index + '"></div>';
+            var tabbedContentHtml = '<div role="tabpanel" class="tab-pane ' + active + '" id="table-results-' + rrType + '"></div>';
             $(".records-tabbed-content").find('.tab-content').append(tabbedContentHtml);
 
+            // Execute the RR render
+            if (rrType === 'A') {
+                renderA(records);
+            } else if (rrType === 'AAAA') {
+                renderAAAA()(records);
+            } else if (rrType === 'NS') {
+                renderNS()(records);
+            } else if (rrType === 'MX') {
+                renderMX()(records);
+            } else if (rrType === 'TXT') {
+                renderTXT()(records);
+            } else if (rrType === 'SOA') {
+                renderSOA()(records);
+            } else if (rrType === 'CNAME') {
+                renderCNAME()(records);
+            }
+
             $.each(value, function( key, record ){
-                $('#table-results-' + index).append(record + '<br />');
+                $('#table-results-' + rrType).append(record + '<br />');
             });
 
             if (active !== '') {
                 active = ''
             }
         });
+    }
+
+    function renderA(records)
+    {
+        log('Rendering all A records');
+        $.each(records, function( key, record ){
+            $('#table-results-A').append(record.address + ' ' + record.location + '<br />');
+        });
+    }
+
+    function renderAAAA(records)
+    {
+        log('Rendering all AAAA records');
+        $.each(records, function( key, record ){
+            $('#table-results-AAAA').append(record.address + ' ' + record.location + '<br />');
+        });
+    }
+
+    function renderNS(records)
+    {
+        log('Rendering all NS records');
+    }
+
+    function renderMX(records)
+    {
+        log('Rendering all MX records');
+    }
+
+    function renderTXT(records)
+    {
+        log('Rendering all TXT records');
+    }
+
+    function renderSOA(records)
+    {
+        log('Rendering all SOA records');
+    }
+
+    function renderCNAME(records)
+    {
+        log('Rendering all CNAME records');
     }
 
 });
